7436396e0ce78a410a8fa7369dd5ee52c00507c5
Elyor Yadgarov <eyodgorov@gmail.com>
2025-10-03 10:01:49 -0400

    Done

--- app.js
+++ app.js
@@ -4,7 +4,33 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
 
 // Exercise 2
-
+const firstName="Elyor";
+lastName="Yadgarov";
+age=41;
+console.log(`${firstName} ${lastName} age is: ${age}`);
 // Exercise 3
+let language="JavaScript";
+let createdYear=1995;
+let isCaseSensetive=true;
+console.log(`${language} is created ${createdYear} case sensetive? ${isCaseSensetive}`);
+
+// Exercise 4
+let price = 19.99;
+let isOnSale =false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+console.log(`Price: ${price} on sale now ${isOnSale} discount percentage ${salePercentage}, stock: ${stock}, is in stock? ${inStock}, selectid size: ${selectedSize}`);
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+console.log(`Book name: ${title} author: ${author} how many page? ${pageCount}, where at now? ${bookmark}, is it readen ${hasRead}`);
